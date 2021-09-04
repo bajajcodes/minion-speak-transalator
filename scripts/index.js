aa20ef317740a6e91dd67f23ef8040b9063ed0f7
shubham bajaj <shubhambajaj32@gmail.com>
2021-09-04 23:25:11 +0530

    feat:base functionality done for all

---
+++ scripts/index.js
@@ -0,0 +1,54 @@
+const input_text = document.querySelector("#input-text");
+const transalte_btn = document.querySelector(".btn-translate");
+const minion_text = document.querySelector("#minion-text");
+
+const API_BASE_URL_END_POINT = "https://api.funtranslations.com/translate/";
+let api_transaltion_end_point = "minion.json"
+let minion_speak = "";
+
+function updateText(txt){
+    minion_text.innerText = txt;    
+}
+
+ function getMinionText(user_text){
+
+    let encoded_text = encodeURI(user_text);
+    let request_url = `${API_BASE_URL_END_POINT}`+`${api_transaltion_end_point}`+`?text=${encoded_text}`;
+    minion_speak = "";
+
+    fetch(request_url)
+        .then( (response) => {
+            return response.json()
+        })
+        .then( (data) => {
+            if(!data.contents){
+                alert(data.error.message);
+            }else{
+                minion_speak = datat.contents.translated;
+            }
+        })
+        .catch(excep => {
+            alert("Sorry for the exception...");
+            console.log(excep);
+        
+        })
+
+    return minion_speak;
+}
+
+function transalte(e){
+    let user_text = input_text.value;
+
+    if(user_text){
+        minion_text.innerText = "";
+        minion_speak = getMinionText(user_text);
+    }
+
+    if(minion_speak){
+        updateText(minion_speak);
+    }else{
+        input_text.value = "";
+    }
+}
+
+transalte_btn.addEventListener("click",transalte);  
